402adccc2829cadf60e289ee906748d397011b17
puhrome <phirom.yim@fullsail.edu>
2014-05-15 19:20:12 -0400

    Added Additional Notes
    More notes added to JS

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Expressions Personal Created by Phirom Yim on 5/15/14.
+ */
+//Using an Array to Calculate the average amount of hours I spend a week riding my bike
+
+
+var bikeRide = [ 0.75, 0.50, 1.0, 2.0, 0.75, 1.50, 0.50];//using arrays for numbers
+
+var total = bikeRide[0] + bikeRide[1] + bikeRide[2] + bikeRide [3] + bikeRide [4] + bikeRide [5] + bikeRide [6];//name of the array then [ ] and the index number
+console.log(total);//print to console the amount of total hours in array
+var average = total/7;//divide the array access notations by 7 days in a week
+console.log(average);//print to add array access notations
+
+alert("You average " + average + " " + "hour(s) a week in riding your bike");//creating alert and concatenation
